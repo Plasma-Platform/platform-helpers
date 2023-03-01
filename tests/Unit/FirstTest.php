c2e2244c631ee8344f91046cc12d4117d48d587f
dima.novoseltsev <dima.nowoseltsew@gmail.com>
2023-03-01 17:44:21 +0200

    add base class for unit tests codeception

---
+++ tests/Unit/FirstTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class FirstTest extends TestCase
+{
+    public function test1()
+    {
+        self::assertEquals(1, 1);
+    }
+}
